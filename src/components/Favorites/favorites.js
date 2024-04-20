ada5478d1e038ff3b06513a77231818fb2ed82fd
Bruna Varela <brunavarela1@hotmail.com>
2024-04-20 11:42:24 -0300

    feat: create favorites heart interections

--- src/components/Favorites/favorites.js
+++ src/components/Favorites/favorites.js
@@ -27,24 +27,78 @@ export const DivList = styled.section`
   li {
     display: flex;
     flex-direction: column;
-    align-items: center;
+    align-items: left;
 
     color: var(--dark-gray);
-    cursor: pointer;
+    position: relative;
+
+    img {
+      margin-bottom: 12px;
+      cursor: pointer;
+    }
+
+    p {
+      font-size: 12px;
+      font-weight: 400;
+    }
+
+    h4 {
+      font-weight: 400;
+      margin-top: 16px;
+      margin-bottom: 6px;
+    }
+
+    span {
+      font-size: 14px;
+      text-decoration: line-through;
+    }
   }
 
   .arrowLeft {
+    z-index: 1;
     position: absolute;
-    bottom: 50%;
+    bottom: 55%;
     left: -2%;
     cursor: pointer;
   }
-  
+
   .arrowRight {
+    z-index: 1;
     position: absolute;
-    bottom: 50%;
+    bottom: 55%;
     right: -2%;
     cursor: pointer;
+  }
+`;
+
+export const DivSpan = styled.div`
+  z-index: 10;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  position: absolute;
+  top: 0%;
+  background-color: transparent;
+
+  padding: 10px;
+
+  h5 {
+    padding: 6px 12px;
+    background-color: var(--pink);
+    font-size: 10px;
+    font-weight: 400;
+    color: var(--white);
+  }
+
+  .imgIcon {
+    font-size: 18px;
+    color: var(--dark-gray);
+    cursor: pointer;
+
+    :hover {
+      color: var(--pink);
+    }
   }
 `;
 
--- src/components/Favorites/favorites.jsx
+++ src/components/Favorites/favorites.jsx
@@ -1,6 +1,15 @@
-import { SectionTitle, FavoritesContainer, DivList, DivFooter } from "./favorites";
+import React, { useState } from "react";
+import {
+  SectionTitle,
+  FavoritesContainer,
+  DivList,
+  DivSpan,
+  DivFooter,
+} from "./favorites";
 import arrowLeft from "../../assets/icons/arrow-left.svg";
 import arrowRight from "../../assets/icons/arrow-right.svg";
+import { SlHeart } from "react-icons/sl";
+import { FaHeart } from "react-icons/fa";
 import dots from "../../assets/icons/dots.svg";
 import favorite1 from "../../assets/img/favorite1.jpg";
 import favorite2 from "../../assets/img/favorite2.jpg";
@@ -8,6 +17,47 @@ import favorite3 from "../../assets/img/favorite3.jpg";
 import favorite4 from "../../assets/img/favorite4.jpg";
 
 export const Favorites = () => {
+  const handleClick = (index) => {
+    const updatedHeartClicked = [...heartClicked];
+    updatedHeartClicked[index] = !updatedHeartClicked[index];
+    setHeartClicked(updatedHeartClicked);
+  };
+
+  const [products, setProducts] = useState([
+    {
+      name: "Tênis Clean Urbano - Branco",
+      price: 209.9,
+      sale: 169.9,
+      paymentOption: "ou 6x de R$ 28,31 sem juros",
+      image: favorite1,
+    },
+    {
+      name: "Sandália Soft Metalizado - Preta",
+      price: 209.9,
+      sale: 169.9,
+      paymentOption: "ou 6x de R$ 28,31 sem juros",
+      image: favorite2,
+    },
+    {
+      name: "Sapatilha Elegance - Rose",
+      price: 209.9,
+      sale: 169.9,
+      paymentOption: "ou 6x de R$ 28,31 sem juros",
+      image: favorite3,
+    },
+    {
+      name: "Plataforma em Nobuck - Whisky",
+      price: 209.9,
+      sale: 169.9,
+      paymentOption: "ou 6x de R$ 28,31 sem juros",
+      image: favorite4,
+    },
+  ]);
+
+  const [heartClicked, setHeartClicked] = useState(
+    Array(products.length).fill(false)
+  );
+
   return (
     <>
       <SectionTitle>Favoritos da semana</SectionTitle>
@@ -17,22 +67,35 @@ export const Favorites = () => {
             <img src={arrowLeft} alt="Seta para esquerda"></img>
           </div>
           <ul>
-            <li>
-              <img src={favorite1} alt="Tênis branco" />
-              <p>Bolsas</p>
-            </li>
-            <li>
-              <img src={favorite2} alt="Sandália preta" />
-              <p>Tênis</p>
-            </li>
-            <li>
-              <img src={favorite3} alt="Sapatilha bege" />
-              <p>Rasteiras</p>
-            </li>
-            <li>
-              <img src={favorite4} alt="vermelho terra" />
-              <p>Plataformas</p>
-            </li>
+            {products.map((product, index) => (
+              <li key={index}>
+                <DivSpan>
+                  <h5>36% OFF</h5>
+                  <div className="imgIcon" onClick={() => handleClick(index)}>
+                    {heartClicked[index] ? (
+                      <FaHeart color="#CF215B" />
+                    ) : (
+                      <SlHeart />
+                    )}
+                  </div>
+                </DivSpan>
+                <img src={product.image} alt="Tênis branco" />
+                <p>{product.name}</p>
+                <h4>
+                  {product.sale.toLocaleString("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  })}{" "}
+                  <span>
+                    {product.price.toLocaleString("pt-BR", {
+                      style: "currency",
+                      currency: "BRL",
+                    })}
+                  </span>
+                </h4>
+                <p>{product.paymentOption}</p>
+              </li>
+            ))}
           </ul>
           <div className="arrowRight">
             <img src={arrowRight} alt="Seta para direita"></img>
